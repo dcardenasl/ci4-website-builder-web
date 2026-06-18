f0ae012af61364f25b7608a38cf4e2c8668268b4
David Cardenas <cine.dcl@gmail.com>
2026-06-18 03:42:11 -0400

    chore(web): update configuration and environment variables for site setup

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,6 +4,15 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 
+// Restrict routes with {locale} to Config\App::$supportedLocales
+$routes->useSupportedLocalesOnly(true);
+
+// Localized routes
+$routes->get('{locale}', 'PageController::home', ['as' => 'home_localized']);
+$routes->get('{locale}/sitemap.xml', 'SitemapController::index', ['as' => 'sitemap_localized']);
+$routes->get('{locale}/(:any)', 'PageController::resolve/$2');
+
+// Fallback non-localized routes (redirected/resolved dynamically)
 $routes->get('/', 'PageController::home', ['as' => 'home']);
 $routes->get('sitemap.xml', 'SitemapController::index', ['as' => 'sitemap']);
 $routes->get('(:any)', 'PageController::resolve/$1');
